8be5f2ff53790bd5ebe6979a6d1b248224cb9cad
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-04-30 12:43:19 -0600

    use logical inline properties on timeline spine for rtl flip

--- src/components/timeline/TimelineSpine.tsx
+++ src/components/timeline/TimelineSpine.tsx
@@ -8,19 +8,14 @@ interface TimelineSpineProps {
 export default function TimelineSpine({ children }: TimelineSpineProps) {
   return (
     <div className="relative w-full max-w-5xl mx-auto px-4 md:px-8 py-12">
-      {/* The vertical spine line */}
       <div
         className={cn(
           'absolute top-0 bottom-0 w-[2px] bg-gold',
-          // Centered on desktop, left-aligned on mobile
-          'left-6 md:left-1/2 md:-translate-x-[1px]'
+          'start-6 md:start-1/2 md:-translate-x-[1px] rtl:md:translate-x-[1px]'
         )}
         aria-hidden="true"
       />
-      {/* Timeline content placed along the spine */}
-      <div className="relative">
-        {children}
-      </div>
+      <div className="relative">{children}</div>
     </div>
   );
 }
